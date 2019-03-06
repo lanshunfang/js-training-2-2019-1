40eaa9a6ba01f48b2d288fc914a89bdf62bd971a
wangqiangfu <wangqiangfu2013@126.com>
2019-03-06 14:13:50 +0800

    edit register.html and script.js

--- javascript/qiangfu/2019-2-25/script.js
+++ javascript/qiangfu/2019-2-25/script.js
@@ -3,39 +3,39 @@
  * @author wqf
  */
 function submitRegister(){
-    var email= document.getElementById('inputEmail').value;
-    var pwd= document.getElementById('inputPassword').value;
-    var imagePath= document.getElementById('inputFile').value;
-    if(document.getElementById('inputCheckMeOut').checked){
-        ifCheckMeOut='Y';
-    }
-    else{
-        ifCheckMeOut='N';
-    }
-   // var ifCheckMeOut= document.getElementById('inputCheckMeOut').value;
-    var registerInfomation = new Array();
-    var registerJson = "";
+  var email = document.getElementById('inputEmail').value;
+  var pwd = document.getElementById('inputPassword').value;
+  var imagePath = document.getElementById('inputFile').value;
+  if(document.getElementById('inputCheckMeOut').checked){
+    ifCheckMeOut = 'Y';
+  }
+  else{
+    ifCheckMeOut = 'N';
+  }
+  // var ifCheckMeOut= document.getElementById('inputCheckMeOut').value;
+  var registerInfomation = new Array();
+  var registerJson = "";
 
-	if (email ==='') {
-		alert('You must input an Email!');
-		document.getElementById('inputEmail').focus();
-	}
-	else{
-        registerInfomation.push({email: email});
-        registerInfomation.push({pwd:pwd});
-        registerInfomation.push({imagePath:imagePath});
-        registerInfomation.push({ifCheckMeOut:ifCheckMeOut});
-        registerJson = JSON.stringify(registerInfomation);
+  if (email ==='') {
+    alert('You must input an Email!');
+    document.getElementById('inputEmail').focus();
+  }
+  else{
+    registerInfomation.push({email: email});
+    registerInfomation.push({pwd:pwd});
+    registerInfomation.push({imagePath:imagePath});
+    registerInfomation.push({ifCheckMeOut:ifCheckMeOut});
+    registerJson = JSON.stringify(registerInfomation);
 
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function(){
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                alert(xmlHttp.responseText);
-            }
-        }
-        xmlHttp.open('POST','/', true);
-        xmlHttp.send();
-	}
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function(){
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+        alert(xmlHttp.responseText);
+      }
+    }
+    xmlHttp.open('POST','/', true);
+    xmlHttp.send();
+  }
 };
 
 /**
@@ -45,19 +45,18 @@ function submitRegister(){
  * @param {file控件的值} obj 
  */
 function imagePreview(obj){
-	if (obj.value ==='') {
-		document.getElementById('divImage').style.display='none';
-	}
-	else{
-		document.getElementById('divImage').style.display='block';
-		var reads= new FileReader();        
-		file=document.getElementById('inputFile').files[0];
-        reads.readAsDataURL(file);
-        reads.onload=function (e) {
-        	document.getElementById('imgPreview').src=this.result;
-        }
-	}
-	
+  if (obj.value ==='') {
+    document.getElementById('divImage').style.display = 'none';
+  }
+  else{
+    document.getElementById('divImage').style.display = 'block';
+    var reads = new FileReader();    
+    file = document.getElementById('inputFile').files[0];
+    reads.readAsDataURL(file);
+    reads.onload = function(e){
+    document.getElementById('imgPreview').src = this.result;
+    }
+  }  
 };
 
 /**
@@ -65,87 +64,84 @@ function imagePreview(obj){
  * @author wqf
  */
 function validateEmail(){
-	var email= document.getElementById('inputEmail').value;
-	var reg = new RegExp('(^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$)');
-	if (!reg.test(email)) {
-		document.getElementById('btnSubmit').setAttribute('disabled','disabled');
-		document.getElementById('btnSubmit').style.background='#eee';
-		document.getElementById('btnSubmit').style.color='#888';
-		document.getElementById('errorLabel').innerHTML='Incorrect email format!';
-	}
-	else{
-		document.getElementById('btnSubmit').removeAttribute('disabled');
-		document.getElementById('errorLabel').innerHTML='';
-		document.getElementById('btnSubmit').style.background='#fff';
-		document.getElementById('btnSubmit').style.color='black';
-	}
-	
+  var email = document.getElementById('inputEmail').value;
+  var reg = new RegExp('(^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$)');
+  if (!reg.test(email)) {
+    document.getElementById('btnSubmit').setAttribute('disabled','disabled');
+    document.getElementById('btnSubmit').style.background = '#eee';
+    document.getElementById('btnSubmit').style.color = '#888';
+    document.getElementById('errorLabel').innerHTML = 'Incorrect email format!';
+  }
+  else{
+    document.getElementById('btnSubmit').removeAttribute('disabled');
+    document.getElementById('errorLabel').innerHTML = '';
+    document.getElementById('btnSubmit').style.background = '#fff';
+    document.getElementById('btnSubmit').style.color = 'black';
+  }  
 };
 
 window.onbeforeunload = function(){
-    var email= document.getElementById('inputEmail').value;
-    var pwd= document.getElementById('inputPassword').value;
-    var imagePath= document.getElementById('inputFile').value;
-    if(document.getElementById('inputCheckMeOut').checked){
-        ifCheckMeOut='Y';
-    }
-    else{
-        ifCheckMeOut='N';
-    }
-
-    window.sessionStorage.setItem('EMAIL',email);
-    window.sessionStorage.setItem('PWD',pwd);
-    if (imagePath !== null){
-       var file=document.getElementById('inputFile').files[0];
-       window.sessionStorage.setItem('IMAGEPATH',file);
-    }    
-    window.sessionStorage.setItem('IFCHECKMEOUT',ifCheckMeOut);
+  var email = document.getElementById('inputEmail').value;
+  var pwd = document.getElementById('inputPassword').value;
+  var imagePath = document.getElementById('inputFile').value;
+  if(document.getElementById('inputCheckMeOut').checked){
+    ifCheckMeOut = 'Y';
+  }
+  else{
+    ifCheckMeOut = 'N';
+  }
+  window.sessionStorage.setItem('EMAIL',email);
+  window.sessionStorage.setItem('PWD',pwd);
+  if (imagePath !== null){
+     var file=document.getElementById('inputFile').files[0];
+     window.sessionStorage.setItem('IMAGEPATH',file);
+  }  
+  window.sessionStorage.setItem('IFCHECKMEOUT',ifCheckMeOut);
 }
 
 window.onload = function(){
-        var email= window.sessionStorage.getItem('EMAIL');
-        var pwd= window.sessionStorage.getItem('PWD');
-        var imagePath= window.sessionStorage.getItem('IMAGEPATH');
-        var ifCheckMeOut= window.sessionStorage.getItem('IFCHECKMEOUT');
-    
-        if(email !== '' && email !== null){
-            document.getElementById('inputEmail').value = email;
-        }
-        if(pwd !== '' && pwd !== null){
-            document.getElementById('inputPassword').value = pwd;
-        }
-        /*if(imagePath !== '' && imagePath !== null){
-            document.getElementById('divImage').style.display='block';
-		    var reads= new FileReader();        
-            reads.readAsDataURL(imagePath);
-            reads.onload=function (e) {
-        	    document.getElementById('imgPreview').src=this.result;
-            }
-        }
-        */
-        if(ifCheckMeOut !== '' && ifCheckMeOut !== null){
-            if(ifCheckMeOut === 'Y'){
-                document.getElementById('inputCheckMeOut').setAttribute('checked','checked');
-            }
-            else{
-                document.getElementById('inputCheckMeOut').removeAttribute('checked');
-            }
-        }
-   }
+  var email = window.sessionStorage.getItem('EMAIL');
+  var pwd = window.sessionStorage.getItem('PWD');
+  var imagePath = window.sessionStorage.getItem('IMAGEPATH');
+  var ifCheckMeOut = window.sessionStorage.getItem('IFCHECKMEOUT');
+  if(email !== '' && email !== null){
+    document.getElementById('inputEmail').value = email;
+  }
+  if(pwd !== '' && pwd !== null){
+    document.getElementById('inputPassword').value = pwd;
+  }
+    /*if(imagePath !== '' && imagePath !== null){
+      document.getElementById('divImage').style.display='block';
+    var reads= new FileReader();    
+      reads.readAsDataURL(imagePath);
+      reads.onload=function (e) {
+        document.getElementById('imgPreview').src=this.result;
+      }
+    }
+    */
+  if(ifCheckMeOut !== '' && ifCheckMeOut !== null){
+    if(ifCheckMeOut === 'Y'){
+      document.getElementById('inputCheckMeOut').setAttribute('checked','checked');
+    }
+    else{
+      document.getElementById('inputCheckMeOut').removeAttribute('checked');
+    }
+  }
+}
 
-   function previewOrEdit(){
-        if(document.getElementById('btnPreview').innerHTML =='预览'){
-            document.getElementById('inputEmail').setAttribute('disabled','disabled');
-            document.getElementById('inputBirthday').setAttribute('disabled','disabled');
-            document.getElementById('inputPassword').setAttribute('disabled','disabled');
-            document.getElementById('inputFile').setAttribute('disabled','disabled');
-            document.getElementById('btnPreview').innerHTML ='编辑';
-        }
-        else{
-            document.getElementById('inputEmail').removeAttribute('disabled');
-            document.getElementById('inputBirthday').removeAttribute('disabled');
-            document.getElementById('inputPassword').removeAttribute('disabled');
-            document.getElementById('inputFile').removeAttribute('disabled');
-            document.getElementById('btnPreview').innerHTML ='预览';
-        }
-   }
+function previewOrEdit(){
+  if(document.getElementById('btnPreview').innerHTML == '预览'){
+    document.getElementById('inputEmail').setAttribute('disabled','disabled');
+    document.getElementById('inputBirthday').setAttribute('disabled','disabled');
+    document.getElementById('inputPassword').setAttribute('disabled','disabled');
+    document.getElementById('inputFile').setAttribute('disabled','disabled');
+    document.getElementById('btnPreview').innerHTML = '编辑';
+  }
+  else{
+    document.getElementById('inputEmail').removeAttribute('disabled');
+    document.getElementById('inputBirthday').removeAttribute('disabled');
+    document.getElementById('inputPassword').removeAttribute('disabled');
+    document.getElementById('inputFile').removeAttribute('disabled');
+    document.getElementById('btnPreview').innerHTML = '预览';
+  }
+}
